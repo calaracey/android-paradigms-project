3014bdb294806dfef42b706045f2e6204bee2747
Owen Gallahue <ogallahu@nd.edu>
2019-09-22 21:16:31 -0400

    added Weather class

---
+++ Weather.java
@@ -0,0 +1,110 @@
+#import <"string.h>
+
+class Weather extends prJSONfromURL{
+
+        String name;
+        String cloudDescription;
+        float tempature;
+        int humidity;
+        int pressure;
+        int visibility;
+        float windSpeed;
+        long sunset;
+        long sunrise;
+
+        
+
+        //Constrcutor
+        Weather(){
+        }
+
+        Weather(String name, float temp, String clouds, int humidity, int pressure, int visibility, float wind, long sunrise, long sunset){
+            this.setName(name);
+            this.setTempature(temp);
+            this.setClouds(clouds);
+            this.setHumidity(humidity);
+            this.setPressure(pressure);
+            this.setVisibility(visibility);
+            this.setWindSpeed(wind);
+            this.setSunriseTime(sunrise);
+            this.setSunsetTime(sunset);
+        }
+
+        String getName(){
+            return this.name;
+        }
+
+        void setName(String newName) {
+            this.name = newName;
+        }
+
+        int getTempature() {
+            return this.tempature;    
+        }
+
+        void setTempature(float newTemp) {
+            this.tempature = newTemp;
+        }
+
+        String getClouds() {
+            return this.cloudDescription;    
+        }
+
+        void setClouds(String newClouds); {
+            this.cloudDescription = NewClouds;
+        }
+
+        int getHumidity() {
+            return this.humidity;    
+        }
+
+        void setHumidity(int NewHumidity) {
+            this.humidity = NewHumidity;
+        }
+
+        int getPressure() {
+            return this.pressure;
+        }
+
+        void setPressure(String NewPressure){
+            this.pressure = NewPressure;
+        }
+        
+        int getGenre() {
+            return this.visibility;
+        }
+
+        void setVisibility(String NewVisibility){
+            this.visibility = NewVisibility;
+        }
+        
+        float getWindSpeed() {
+            return this.windSpeed;
+        }
+
+        void setWindSpeed(float NewWindSpeed){
+            this.windSpeed = NewWindSpeed;
+        }
+
+        long getSunset() {
+            return this.sunset;
+        }
+
+        void setSunset(long NewSunset){
+            this.sunset = NewSunset;
+        }
+
+        long getSunrise() {
+            return this.sunrise;
+        }
+
+        void setSunrise(long NewSunrise){
+            this.sunrise = NewSunrise;
+        }
+        @Override
+        public String toString() {
+            String str = "Title: "+ getName() + " Temp " + getTempature() + " wind: " + getWindSpeed();
+            return str; 
+        }
+
+}
